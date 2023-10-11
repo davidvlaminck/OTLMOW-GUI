d54923ae7db202b614fd206e864e02f060c13e2f
Jasper Berton <jasper.berton@student.hogent.be>
2023-10-11 16:31:33 +0200

    zeer rudimentaire vormgeving van startscherm

--- GUI/home_screen.py
+++ GUI/home_screen.py
@@ -1,31 +1,57 @@
-from PyQt6.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel, QHBoxLayout
+from PyQt6.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel, QHBoxLayout, QVBoxLayout
 from PyQt6.QtCore import Qt
+
+
 class HomeScreen(QWidget):
     def __init__(self):
         super().__init__()
 
-        #Create GridLayout
-        grid = QGridLayout()
-        Top = QHBoxLayout()
 
-        #Create the header
+        # Create GridLayout
+        grid = QGridLayout()
+        Header = QHBoxLayout()
+        Header.setProperty('class', 'Header')
+
+        # Create the header
         title = QLabel('OTLWizard')
-        Top.addWidget(title)
+        Header.addWidget(title)
         button = QPushButton('New Project')
-        Top.addWidget(button)
-        Top.setAlignment(button, Qt.AlignmentFlag.AlignLeft)
+        Header.addWidget(button)
+        Header.setAlignment(button, Qt.AlignmentFlag.AlignLeft)
         User_pref = QHBoxLayout()
         settings = QPushButton('Settings')
         User_pref.addWidget(settings)
         help = QPushButton('Help')
         User_pref.addWidget(help)
-        Top.addLayout(User_pref)
-        Top.setAlignment(User_pref, Qt.AlignmentFlag.AlignRight)
+        Header.addLayout(User_pref)
+        Header.setAlignment(User_pref, Qt.AlignmentFlag.AlignRight)
 
+        # Create a table
+        table = QVBoxLayout()
+        top = QHBoxLayout()
+        top.addWidget(QLabel('Eigen Referentie:'))
+        top.addWidget(QLabel('Bestek - (dienstbevel):'))
+        top.addWidget(QLabel('Subset:'))
+        top.addWidget(QLabel("Laatst bewerkt:"))
+        table.addLayout(top)
 
-        grid.addLayout(Top, 0, 0, 0, 0, Qt.AlignmentFlag.AlignCenter)
-        grid.setAlignment(Top, Qt.AlignmentFlag.AlignTop)
+        # Create rows in that table
+        for i in range(10):
+            row = QHBoxLayout()
+            row.addWidget(QLabel('Test'))
+            row.addWidget(QLabel('Test'))
+            row.addWidget(QLabel('Test'))
+            row.addWidget(QLabel('Test'))
+            table.addLayout(row)
 
+        # Add the header to the grid
+        grid.addLayout(Header, 0, 0, 0, 0)
+        # Align the header to the top
+        grid.setAlignment(Header, Qt.AlignmentFlag.AlignTop)
+        # Add the table to the grid
+        grid.addLayout(table, 1, 0, 2, 0, Qt.AlignmentFlag.AlignTop)
 
         self.setLayout(grid)
+        with open('custom.css', 'r') as file:
+            self.setStyleSheet(file.read())
         self.show()
--- domain/main.py
+++ domain/main.py
@@ -2,11 +2,14 @@ import sys
 
 from PyQt6.QtWidgets import QApplication, QWidget
 from GUI.home_screen import HomeScreen
-
+from qt_material import apply_stylesheet
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    with open('custom.css', 'r') as file:
+        app.setStyleSheet(file.read())
     window = HomeScreen()
     window.resize(1920, 1080)
     window.setWindowTitle("OTLWizard")
+    apply_stylesheet(app, theme='custom_theme.xml', css_file='custom.css')
     sys.exit(app.exec())
