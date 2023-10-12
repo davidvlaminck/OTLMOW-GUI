8f2354ac37eec23f30f085c726fb8ea5719ca6a8
Jasper Berton <jasper.berton@student.hogent.be>
2023-10-12 13:26:05 +0200

    werkende tabel met resizing possibilities

--- GUI/home_screen.py
+++ GUI/home_screen.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel, QHBoxLayout, QVBoxLayout
+from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QTableWidget, \
+    QTableWidgetItem, QLineEdit, QHeaderView
 from PyQt6.QtCore import Qt
 
 
@@ -6,13 +7,13 @@ class HomeScreen(QWidget):
     def __init__(self):
         super().__init__()
 
-
-        # Create GridLayout
-        grid = QGridLayout()
-        Header = QHBoxLayout()
-        Header.setProperty('class', 'Header')
+        # Vertical layout
+        vert = QVBoxLayout()
 
         # Create the header
+        headWrapper = QWidget()
+        headWrapper.setProperty('class', 'header')
+        Header = QHBoxLayout()
         title = QLabel('OTLWizard')
         Header.addWidget(title)
         button = QPushButton('New Project')
@@ -25,50 +26,52 @@ class HomeScreen(QWidget):
         User_pref.addWidget(help)
         Header.addLayout(User_pref)
         Header.setAlignment(User_pref, Qt.AlignmentFlag.AlignRight)
+        headWrapper.setLayout(Header)
 
-        # Create a table
-        table = QVBoxLayout()
-        top = QHBoxLayout()
-        titel1 = QLabel('Eigen Referentie:')
-        titel1.setProperty('class', 'titel')
-        top.addWidget(titel1)
-        titel2 = QLabel('Bestek - (dienstbevel):')
-        titel2.setProperty('class', 'titel')
-        top.addWidget(titel2)
-        titel3 = QLabel('Subset:')
-        titel3.setProperty('class', 'titel')
-        top.addWidget(titel3)
-        titel4 = QLabel('Laatst bewerkt:')
-        titel4.setProperty('class', 'titel')
-        top.addWidget(titel4)
-        table.addLayout(top)
-        table.setSpacing(0)
+        # Search bar
+        searchWrapper = QWidget()
+        searchWrapper.setProperty('class', 'search')
+        search = QHBoxLayout()
+        inputField = QLineEdit()
+        search.addWidget(inputField)
+        searchbutton = QPushButton('Search')
+        search.addWidget(searchbutton)
+        searchWrapper.setLayout(search)
 
-        # Create rows in that table
-        for i in range(10):
-            row = QHBoxLayout()
-            row1 = QLabel('Test')
-            row1.setProperty('class', 'row')
-            row.addWidget(row1)
-            row2 = QLabel('Test')
-            row2.setProperty('class', 'row')
-            row.addWidget(row2)
-            row3 = QLabel('Test')
-            row3.setProperty('class', 'row')
-            row.addWidget(row3)
-            row4 = QLabel('Test')
-            row4.setProperty('class', 'row')
-            row.addWidget(row4)
-            table.addLayout(row)
+        # Create the table with QTableView
+        table = QTableWidget()
+        table.setRowCount(4)
+        table.verticalHeader().setVisible(False)
+        table.setColumnCount(4)
 
-        # Add the header to the grid
-        grid.addLayout(Header, 0, 0, 0, 0)
-        # Align the header to the top
-        grid.setAlignment(Header, Qt.AlignmentFlag.AlignTop)
-        # Add the table to the grid
-        grid.addLayout(table, 1, 0, 2, 0, Qt.AlignmentFlag.AlignTop)
+        for column in range(table.columnCount()):
+            table.horizontalHeader().setSectionResizeMode(column, QHeaderView.ResizeMode.Stretch)
+            #table.setColumnWidth(column, 474)
+        table.setShowGrid(False)
+        # Zorgt ervoor dat selectie op row is niet op cell
+        table.setSelectionBehavior(QTableWidget.SelectionBehavior.SelectRows)
+        # Zorgt ervoor dat de table niet editable is
+        table.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
+        table.resizeRowsToContents()
+        table.setHorizontalHeaderLabels(['Eigen referentie:', 'Bestek - (Dienstbevel)', 'Subset', 'Laatst bewerkt'])
+        # ALign titles of header to the left
+        table.horizontalHeader().setDefaultAlignment(Qt.AlignmentFlag.AlignLeft)
 
-        self.setLayout(grid)
-        with open('custom.css', 'r') as file:
-            self.setStyleSheet(file.read())
+        # add data to the table
+        for j in range(4):
+            for i in range(4):
+                item = QTableWidgetItem()
+                item.setText('test')
+                table.setItem(j, i, item)
+
+        # add header to the vertical layout
+        vert.addWidget(headWrapper)
+        vert.addSpacing(39)
+        # add searchbar to the vertical layout
+        vert.addWidget(searchWrapper)
+        vert.addSpacing(43)
+        # add table to the vertical layout
+        vert.addWidget(table)
+
+        self.setLayout(vert)
         self.show()
--- domain/main.py
+++ domain/main.py
@@ -6,10 +6,9 @@ from qt_material import apply_stylesheet
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    with open('custom.css', 'r') as file:
+    with open('custom.qss', 'r') as file:
         app.setStyleSheet(file.read())
     window = HomeScreen()
     window.resize(1920, 1080)
     window.setWindowTitle("OTLWizard")
-    apply_stylesheet(app, theme='custom_theme.xml', css_file='custom.css')
     sys.exit(app.exec())
