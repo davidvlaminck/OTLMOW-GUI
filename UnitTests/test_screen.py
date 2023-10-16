8e5e7181c173b91780c4894f686afe88a1c9b04b
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-10-16 10:19:13 +0200

    init home screen

---
+++ UnitTests/test_screen.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from GUI.home_screen import HomeScreen
+
+
+def test_init_home_screen():
+    # launch an instance of HomeScreen and assert that it is not None
+
+    app = QApplication(sys.argv)
+    window = HomeScreen()
+    assert window is not None
+    window.close()
+    app.quit()
+
+# if needed, alternative solution (2nd!):
+# https://stackoverflow.com/questions/70267550/testing-pyqt-application-with-pytest
