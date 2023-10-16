1f9e648a62c4f2a023f75f1472f575ce3f165bb9
Jasper Berton <jasper.berton@student.hogent.be>
2023-10-16 17:52:03 +0200

    test van home screen updaten

--- UnitTests/init_screen_test.py
+++ UnitTests/init_screen_test.py
@@ -1,14 +1,18 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from GUI.home_screen import HomeScreen
+from Domain.database import Database
 
 
 def test_init_home_screen():
     # launch an instance of HomeScreen and assert that it is not None
     app = QApplication(sys.argv)
-    window = HomeScreen()
+    db = Database()
+    db.create_connection(":memory:")
+    window = HomeScreen(db)
     assert window is not None
     window.close()
+    db.close_connection()
     app.quit()
 
 # if needed, alternative solution (2nd!):
