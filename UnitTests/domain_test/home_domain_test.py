890bdcdd03a292819e076e6aa53d4a2ccc2d6d2d
Jasper Berton <jasper.berton@student.hogent.be>
2023-10-24 15:30:45 +0200

    fix home domain test

--- UnitTests/domain_test/home_domain_test.py
+++ UnitTests/domain_test/home_domain_test.py
@@ -1,12 +1,12 @@
-from Domain.language_settings import LanguageSettings
+from Domain.language_settings import return_language
 from Domain.home_domain import HomeDomain
 from Domain.database import Database
 
 
 def test_get_amount_of_rows():
     db = Database()
-    language_settings = LanguageSettings()
+    _ = return_language('../../locale/')
     db.create_connection(":memory:")
-    home_domain = HomeDomain(db, language_settings)
+    home_domain = HomeDomain(db, _)
     assert home_domain.get_amount_of_rows() == 0
     db.close_connection()
