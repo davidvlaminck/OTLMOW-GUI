90ea1ee4c1b08d11c94e0cd56fdf8d6033ade567
Jasper Berton <jasper.berton@student.hogent.be>
2023-11-07 10:06:00 +0100

    update modelbuilder

--- Domain/model_builder.py
+++ Domain/model_builder.py
@@ -1,25 +1,20 @@
 from pathlib import Path
 
-from otlmow_modelbuilder.OSLOInMemoryCreator import OSLOInMemoryCreator
-from otlmow_modelbuilder.SQLDataClasses.OSLOCollector import OSLOCollector
-from otlmow_modelbuilder.SQLDbReader import SQLDbReader
+from otlmow_modelbuilder.OSLOCollector import OSLOCollector
 
 
 class ModelBuilder:
 
     def __init__(self, path):
-        file_location = Path(path)
-        sql_reader = SQLDbReader(file_location)
-        self.creator = OSLOInMemoryCreator(sql_reader)
-        collector = OSLOCollector(self.creator)
-        collector.collect()
+        self.collector = OSLOCollector(Path(path))
+        self.collector.collect_all()
 
     def get_all_classes(self):
-        return self.creator.get_all_classes()
+        return self.collector.classes
 
 
 if __name__ == '__main__':
-    creator = ModelBuilder(path='C:/#schoolwerk/Jaar III/Stage/testDb/FlitspaalTest.db')
-    test = ModelBuilder.get_all_classes(creator)
+    builder = ModelBuilder(path='C:/#schoolwerk/Jaar III/Stage/testDb/FlitspaalTest.db')
+    test = builder.get_all_classes()
     for i in test:
-        print(i)
+        print(i.name)
