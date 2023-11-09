cf79b40ea93f4418718298061f591140b454fa60
Jasper Berton <jasper.berton@student.hogent.be>
2023-11-09 17:56:45 +0100

    lists on relations page

--- GUI/Screens/relation_change_screen.py
+++ GUI/Screens/relation_change_screen.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import qtawesome as qta
+from PyQt6.QtCore import Qt
 
-from PyQt6.QtWidgets import QVBoxLayout, QFrame, QHBoxLayout, QPushButton, QWidget
+from PyQt6.QtWidgets import QVBoxLayout, QFrame, QHBoxLayout, QPushButton, QWidget, QLineEdit, QLabel, QListWidget
 
 from Domain.language_settings import return_language
 from GUI.Screens.screen import Screen
@@ -39,6 +41,10 @@ class RelationChangeScreen(Screen):
         window.setProperty('class', 'background-box')
         window_layout = QVBoxLayout()
         window_layout.addWidget(self.navigation_buttons())
+        window_layout.addSpacing(10)
+        window_layout.addWidget(self.input_file_field())
+        window_layout.addSpacing(10)
+        window_layout.addWidget(self.listbox())
         window.setLayout(window_layout)
         return window
 
@@ -59,6 +65,69 @@ class RelationChangeScreen(Screen):
         frame_layout.addSpacing(200)
         frame.setLayout(frame_layout)
         return frame
+
+    def input_file_field(self):
+        frame = QFrame()
+        frame_layout = QHBoxLayout()
+        input_field = QLineEdit()
+        input_field.setReadOnly(True)
+        input_field.setPlaceholderText(self._('choose_file'))
+        input_file_button = QPushButton()
+        input_file_button.setIcon(qta.icon('mdi.folder-open-outline'))
+        frame_layout.addWidget(input_field)
+        frame_layout.addWidget(input_file_button)
+        frame_layout.addStretch()
+        frame.setLayout(frame_layout)
+        return frame
+
+    def class_list(self):
+        frame = QFrame()
+        frame_layout = QVBoxLayout()
+        class_label = QLabel()
+        class_label.setText(self._('class_list'))
+        class_list = QListWidget()
+        class_list.setProperty('class', 'list')
+
+        frame_layout.addWidget(class_label)
+        frame_layout.addWidget(class_list)
+        frame.setLayout(frame_layout)
+        return frame
+
+    def relations_list(self):
+        frame = QFrame()
+        frame_layout = QVBoxLayout()
+        relations_label = QLabel()
+        relations_label.setText(self._('relations_list'))
+        class_list = QListWidget()
+        class_list.setProperty('class', 'list')
+
+        frame_layout.addWidget(relations_label)
+        frame_layout.addWidget(class_list)
+        frame.setLayout(frame_layout)
+        return frame
+
+    def existing_relations_list(self):
+        frame = QFrame()
+        frame_layout = QVBoxLayout()
+        existing_rel_label = QLabel()
+        existing_rel_label.setText(self._('existing_relations_list'))
+        class_list = QListWidget()
+        class_list.setProperty('class', 'list')
+
+        frame_layout.addWidget(existing_rel_label)
+        frame_layout.addWidget(class_list)
+        frame.setLayout(frame_layout)
+        return frame
+
+    def listbox(self):
+        frame = QFrame()
+        frame_layout = QHBoxLayout()
+        frame_layout.addWidget(self.class_list())
+        frame_layout.addWidget(self.relations_list())
+        frame_layout.addWidget(self.existing_relations_list())
+        frame.setLayout(frame_layout)
+        return frame
+
 
     def reset_ui(self, _):
         self._ = _
