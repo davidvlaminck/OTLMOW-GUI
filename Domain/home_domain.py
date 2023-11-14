3d1e1eb168c49af916bf5438bf86de4a286d3540
Jasper Berton <jasper.berton@student.hogent.be>
2023-11-14 14:09:10 +0100

    last changed updated when subset changes on template screen

--- Domain/home_domain.py
+++ Domain/home_domain.py
@@ -45,6 +45,8 @@ class HomeDomain:
 
     @staticmethod
     def change_subset(project, new_path, dialog, stacked_widget):
+        time_of_alter = datetime.now().date()
+        project.laatst_bewerkt = time_of_alter
         project.subset_path = Path(new_path)
         ProjectFileManager.save_project_to_dir(project)
         stacked_widget.widget(1).tab1.project = project
