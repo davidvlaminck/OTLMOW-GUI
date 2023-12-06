1677907ed1f708068772f7b40bed4191e39822ab
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-12-06 13:22:28 +0100

    unittest to pass

--- UnitTests/domain_test/diff_report_test.py
+++ UnitTests/domain_test/diff_report_test.py
@@ -9,9 +9,19 @@ from UnitTests.project_files_test.OTLWizardProjects.Model.OtlmowModel.Classes.On
 
 model_directory_path = Path(__file__).parent.parent.parent / 'project_files_test' / 'OTLWizardProjects' / 'Model'
 
+
+enum_report_action = {
+    'a': 'attribute added',
+    'r': 'attribute removed',
+    'c': 'attribute changed',
+    'aa': 'asset added'
+}
+
+
 @dataclass
 class ReportItem:
     id: str
+    actie: enum_report_action
     attribute: str
     original_value: str
     new_value: str
@@ -39,8 +49,9 @@ def test_generate_report():
     instance_2_2.testComplexType.testStringField = 'gewijzigde complexe waarde'
 
     instance_1_3 = AllCasesTestClass()
-    instance_1_3.assetId.identificator = '4'
+    instance_1_3.assetId.identificator = '3'
     instance_1_3.isActief = True
+    # TODO bert navragen of dit de bedoeling is
 
     instance_2_4 = AnotherTestClass()
     instance_2_4.assetId.identificator = '4'
@@ -49,16 +60,13 @@ def test_generate_report():
     instance_list_1 = [instance_1_1, instance_1_2, instance_1_3]
     instance_list_2 = [instance_2_1, instance_2_2, instance_2_4]
 
-    report = generate_report(original_data=instance_list_1, new_data=instance_list_2,
-                             model_directory=model_directory_path)
+    report = generate_diff_report(original_data=instance_list_1, new_data=instance_list_2,
+                                  model_directory=model_directory_path)
 
     assert report == [
-        ReportItem(id='2', attribute='isActief', original_value='True', new_value='False'),
-        ReportItem(id='2', attribute='testStringField', original_value='test', new_value='gewijzigd'),
-        ReportItem(id='2', attribute='testComplexType', original_value="{'testStringField':'complexe waarde'}",
+        ReportItem(id='2', actie=enum_report_action.a, attribute='isActief', original_value='True', new_value='False'),
+        ReportItem(id='2', actie=enum_report_action.a, attribute='testStringField', original_value='test', new_value='gewijzigd'),
+        ReportItem(id='2', actie=enum_report_action.a, attribute='testComplexType', original_value="{'testStringField':'complexe waarde'}",
                    new_value="{'gewijzigde complexe waarde'}"),
-        ReportItem(id='4', attribute='isActief', original_value='', new_value='True')
+        ReportItem(id='4', actie=enum_report_action.aa, attribute='', original_value='', new_value='')
     ]
-
-
-
