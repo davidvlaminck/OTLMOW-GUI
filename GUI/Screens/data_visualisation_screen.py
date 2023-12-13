364ff8884310ad6c53fcaa8643bb506424560636
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-12-13 16:32:23 +0100

    added load_assets_and_create_html

--- GUI/Screens/data_visualisation_screen.py
+++ GUI/Screens/data_visualisation_screen.py
@@ -3,7 +3,10 @@ from pathlib import Path
 from PyQt6.QtCore import QUrl
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 from PyQt6.QtWidgets import QVBoxLayout, QLabel, QWidget
+from otlmow_converter.OtlmowConverter import OtlmowConverter
+from otlmow_visuals.PyVisWrapper import PyVisWrapper
 
+from Domain import global_vars
 from GUI.Screens.screen import Screen
 
 ROOT_DIR = Path(__file__).parent
@@ -28,6 +31,7 @@ class DataVisualisationScreen(Screen):
         window_layout = QVBoxLayout()
         title_label = QLabel()
         title_label.setText(self._("Data visualisation"))
+
         view = QWebEngineView()
         html_loc = HTML_DIR / "visuals.html"
         view.setHtml(open(html_loc).read())
@@ -36,4 +40,19 @@ class DataVisualisationScreen(Screen):
         return window
 
     def reset_ui(self, _):
-        pass
+        self.load_assets_and_create_html()
+
+    def load_assets_and_create_html(self):
+        project = global_vars.single_project
+        if project is None:
+            return
+        valid_file_paths = [file.file_path for file in project.templates_in_memory if file.state in ['OK', 'ok']]
+
+        objects_in_memory = []
+        for path in valid_file_paths:
+            objects_in_memory.extend(OtlmowConverter().create_assets_from_file(
+                filepath=Path(path), path_to_subset=project.subset_path))
+
+        html_loc = HTML_DIR / "visuals.html"
+        PyVisWrapper().show(list_of_objects=objects_in_memory, html_path=html_loc, launch_html=False)
+
