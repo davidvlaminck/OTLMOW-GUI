6190ea04b02b4fba097fb7f6a83095f9d5801ae8
Jasper Berton <jasper.berton@student.hogent.be>
2023-12-14 16:19:07 +0100

    refactor van export data domain

--- Domain/export_data_domain.py
+++ Domain/export_data_domain.py
@@ -9,40 +9,55 @@ class ExportDataDomain:
 
     @classmethod
     def generate_files(cls, end_file, project, csv_option, relations_option):
-        logging.debug(f'relations option: {relations_option}')
-        logging.debug(f'project_files in memory: {project.templates_in_memory}')
 
+        objects_in_memory = cls.extract_objects_from_files(project=project)
+
+        if relations_option:
+            assets_in_memory, relations_in_memory = cls.split_relations_and_objects(objects_in_memory)
+            relations_path, assets_path = cls.create_relation_and_class_path(end_file)
+            if relations_in_memory:
+                OtlmowConverter().create_file_from_assets(filepath=relations_path,
+                                                          list_of_objects=relations_in_memory,
+                                                          split_per_type=csv_option)
+            if assets_in_memory:
+                OtlmowConverter().create_file_from_assets(filepath=assets_path, list_of_objects=assets_in_memory,
+                                                          split_per_type=csv_option)
+        else:
+            OtlmowConverter().create_file_from_assets(list_of_objects=objects_in_memory, filepath=Path(end_file),
+                                                      split_per_type=csv_option)
+
+    @classmethod
+    def extract_objects_from_files(cls, project):
         valid_file_paths = [file.file_path for file in project.templates_in_memory if file.state in ['OK', 'ok']]
 
         objects_in_memory = []
         for path in valid_file_paths:
             objects_in_memory.extend(OtlmowConverter().create_assets_from_file(
                 filepath=Path(path), path_to_subset=project.subset_path))
+        return objects_in_memory
 
-        if relations_option:
-            assets_in_memory = []
-            relations_in_memory = []
-            logging.debug(f'objects in memory: {objects_in_memory}')
-            for obj in objects_in_memory:
-                if hasattr(obj, 'source') and isinstance(obj, RelatieObject):
-                    relations_in_memory.append(obj)
-                else:
-                    assets_in_memory.append(obj)
-            parent_directory = Path(end_file).parent
-            relations_file_name = (
-                f"{Path(end_file).stem}_relations{Path(end_file).suffixes[0]}"
-            )
-            logging.debug(f'rel name: {relations_file_name}')
-            assets_file_name = f"{Path(end_file).stem}_assets{Path(end_file).suffixes[0]}"
-            relations_path = Path(parent_directory) / relations_file_name
-            assets_path = Path(parent_directory) / assets_file_name
-            if relations_in_memory:
-                OtlmowConverter().create_file_from_assets(filepath=Path(relations_path),
-                                                          list_of_objects=relations_in_memory,
-                                                          split_per_type=csv_option)
-            if assets_in_memory:
-                OtlmowConverter().create_file_from_assets(filepath=Path(assets_path), list_of_objects=assets_in_memory,
-                                                          split_per_type=csv_option)
-        else:
-            OtlmowConverter().create_file_from_assets(list_of_objects=objects_in_memory, filepath=Path(end_file),
-                                                      split_per_type=csv_option)
+    @classmethod
+    def split_relations_and_objects(cls, objects_in_memory):
+        assets_in_memory = []
+        relations_in_memory = []
+        logging.debug(f'objects in memory: {objects_in_memory}')
+        for obj in objects_in_memory:
+            if hasattr(obj, 'bronAssetId') or isinstance(obj, RelatieObject):
+                relations_in_memory.append(obj)
+            else:
+                assets_in_memory.append(obj)
+        return assets_in_memory, relations_in_memory
+
+    @classmethod
+    def create_relation_and_class_path(cls, end_file):
+        parent_directory = Path(end_file).parent
+        file_stem = Path(end_file).stem
+        file_suffix = Path(end_file).suffixes[0]
+        relations_file_name = (
+            f"{file_stem}_relations{file_suffix}"
+        )
+        assets_file_name = f"{file_stem}_assets{file_suffix}"
+        relations_path = Path(parent_directory) / relations_file_name
+        assets_path = Path(parent_directory) / assets_file_name
+        return relations_path, assets_path
+
