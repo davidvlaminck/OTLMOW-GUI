4201243c7fca685f027cd9d6dd74e132025cc2b3
christiaan <christiaan.vanbergen.btf@gmail.com>
2024-10-03 15:51:28 +0200

    Tests to verify response to bad subset path inputs

--- UnitTests/domain_test/subset_db_test.py
+++ UnitTests/domain_test/subset_db_test.py
@@ -9,6 +9,21 @@ from Exceptions.WrongDatabaseError import WrongDatabaseError
 
 ROOT_DIR = Path(__file__).parent.parent
 
+def test_subset_db_raises_file_runtime_error_when_path_is_db_file_but_not_subset():
+    # UnitTests/domain_test/home_domain_test.py for test
+    pass
+
+
+def test_subset_db_raises_file_runtime_error_when_path_is_bad_file():
+    bad_file_path = Path(ROOT_DIR / 'project_files_test' / 'OTLWizardProjects' / 'Projects' / 'project_1' / 'notSqlLiteFile.db')
+
+    with open(bad_file_path , 'w+') as fp:
+        fp.write("This is a textfile not a sqllite file")
+
+    with pytest.raises(NotASqlliteFileError):
+        SubsetDatabase(Path(bad_file_path))
+
+    os.remove(bad_file_path)
 
 def test_subset_db_raises_file_runtime_error_when_path_is_empty():
     with pytest.raises(NotASqlliteFileError):
