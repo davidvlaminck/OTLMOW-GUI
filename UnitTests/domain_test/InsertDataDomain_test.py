8cbef8ea2b6dc3461966be240c3a38db8aafa225
christiaan <christiaan.vanbergen.btf@gmail.com>
2024-10-15 14:04:45 +0200

    Moved setup_test_project to DomainFixtures.py + docstring

--- UnitTests/domain_test/InsertDataDomain_test.py
+++ UnitTests/domain_test/InsertDataDomain_test.py
@@ -29,28 +29,7 @@ from UnitTests.general_fixtures.DomainFixtures import *
 def root_directory() -> Path:
     return Path(__file__).parent.parent.parent
 
-@fixture
-def setup_test_project(root_directory: Path) -> None:
-    # test_subset_file_path: Path = Path(
-    #     "{0}\\demo_projects\\simpel_vergelijkings_project\\simpele_vergelijkings_subset.db".format(
-    #         str(root_directory)))
-    # project_file_path: Path = Path(
-    #     "{0}\\demo_projects\\simpel_vergelijkings_project\\wizardProject".format(
-    #         str(root_directory)))
 
-    test_subset_file_path: Path = (root_directory / "demo_projects"
-                                   /  "simpel_vergelijkings_project"
-                                   / "simpele_vergelijkings_subset.db")
-    project_file_path: Path = (root_directory / "demo_projects"  /  "simpel_vergelijkings_project"
-                               / "wizardProject")
-
-    global_vars.current_project = Project(project_path=project_file_path,
-                                          subset_path=Path(test_subset_file_path))
-    original_get_otl_wizard_projects_dir = ProjectFileManager.get_otl_wizard_projects_dir
-    ProjectFileManager.get_otl_wizard_projects_dir = Mock(
-        return_value=root_directory / "demo_projects" / "simpel_vergelijkings_project")
-    yield
-    ProjectFileManager.get_otl_wizard_projects_dir = original_get_otl_wizard_projects_dir
 
 @fixture
 def mock_screen(qtbot: QtBot, create_translations) -> InsertDataScreen:
