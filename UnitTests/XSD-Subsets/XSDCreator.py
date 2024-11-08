612227fa609315380bddad46ba8dc78c5650730b
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-11-08 12:21:08 +0100

    improvement

--- UnitTests/XSD-Subsets/XSDCreator.py
+++ UnitTests/XSD-Subsets/XSDCreator.py
@@ -2,32 +2,44 @@ from pathlib import Path
 import xml.etree.cElementTree as ET
 
 from otlmow_converter.DotnotationHelper import DotnotationHelper
+from otlmow_model.OtlmowModel.BaseClasses.BooleanField import BooleanField
+from otlmow_model.OtlmowModel.BaseClasses.DateField import DateField
+from otlmow_model.OtlmowModel.BaseClasses.DateTimeField import DateTimeField
+from otlmow_model.OtlmowModel.BaseClasses.FloatOrDecimalField import FloatOrDecimalField
+from otlmow_model.OtlmowModel.BaseClasses.KeuzelijstField import KeuzelijstField
 from otlmow_model.OtlmowModel.BaseClasses.OTLObject import dynamic_create_type_from_uri, get_attribute_by_name, \
     get_attribute_by_uri, dynamic_create_instance_from_uri
+from otlmow_model.OtlmowModel.BaseClasses.StringField import StringField
+from otlmow_model.OtlmowModel.BaseClasses.TimeField import TimeField
 from otlmow_modelbuilder.OSLOCollector import OSLOCollector
 
 
+def create_type_from_attribute_in_element(element, attr_instance):
+    if attr_instance.field == FloatOrDecimalField:
+        simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:decimal")
+        total_digits = ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}totalDigits", value="19")
+        fraction_digits = ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}fractionDigits", value="10")
+    elif attr_instance.field == BooleanField:
+        simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:boolean")
+    elif attr_instance.field in {StringField, DateField, DateTimeField, TimeField}:
+        simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:string")
+        max_length = ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}maxLength", value="9999")
+    elif issubclass(attr_instance.field, KeuzelijstField):
+        simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:string")
+        element.attrib.update({'default': '-'})
+        max_length = 1
+        for keuze, keuzelijstwaarde in attr_instance.field.options.items():
+            if keuzelijstwaarde.status != 'ingebruik':
+                continue
+            enumeration = ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}enumeration", value=keuze)
+            max_length = max(max_length, len(keuze))
+        max_length = ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}maxLength", value=str(max_length))
+
 class XSDCreator:
-    def _create_starting_xml(self):
-        pass
-
-
-
-
-
-        #           <xs:element name="afmeting_hoogte" minOccurs="0" default="">
-        #             <xs:annotation>
-        #               <xs:documentation>De hoogte in millimeter.</xs:documentation>
-        #             </xs:annotation>
-        #             <xs:simpleType>
-        #               <xs:restriction base="xs:decimal">
-        #                 <xs:totalDigits value="19" />
-        #                 <xs:fractionDigits value="10" />
-        #               </xs:restriction>
-        #             </xs:simpleType>
-        #           </xs:element>
-
-
     @classmethod
     def create_xsd_from_subset(cls, subset_path, xsd_path):
         collector = OSLOCollector(subset_path)
@@ -44,27 +56,34 @@ class XSDCreator:
         tree.write("filename.xml")
 
         ET.register_namespace("xs", 'http://www.w3.org/2001/XMLSchema')
-        ET.register_namespace("fdo", 'http://fdo.osgeo.org/schemas')
         ET.register_namespace("gml", 'http://www.opengis.net/gml')
-        ET.register_namespace("Schema1", 'http://fdo.osgeo.org/schemas/feature/Schema1')
+        #ET.register_namespace("Schema1", 'http://fdo.osgeo.org/schemas/feature/Schema1')
+        ET.register_namespace("fdo", 'http://fdo.osgeo.org/schemas')
 
         classes = collector.classes
         for class_ in classes:
             if class_.abstract:
                 continue
-            class_element = ET.SubElement(schema, "{http://www.w3.org/2001/XMLSchema}element", name=f"OTL_{class_.name}", type=f"Schema1:OTL_{class_.name}Type", abstract="false", substitutionGroup="gml:_Feature")
+            class_element = ET.SubElement(
+                schema, "{http://www.w3.org/2001/XMLSchema}element",
+                attrib={'name': f"OTL_{class_.name}", 'type': f"{{http://fdo.osgeo.org/schemas/feature/Schema1}}OTL_{class_.name}Type",
+                        'abstract': "false", 'substitutionGroup': "{http://www.opengis.net/gml}_Feature"}
+            )
             key = ET.SubElement(class_element, "{http://www.w3.org/2001/XMLSchema}key", name=f"OTL_{class_.name}Key")
             selector = ET.SubElement(key, "{http://www.w3.org/2001/XMLSchema}selector", xpath=f".//OTL_{class_.name}")
             field = ET.SubElement(key, "{http://www.w3.org/2001/XMLSchema}field", xpath="FeatId")
 
             class_instance = dynamic_create_instance_from_uri(class_.objectUri)
 
-            complex_type = ET.SubElement(schema, "{http://www.w3.org/2001/XMLSchema}complexType", name=f"OTL_{class_.name}Type", abstract="false", fdo_geometryName="Geometry")
+            complex_type = ET.SubElement(schema, "{http://www.w3.org/2001/XMLSchema}complexType",
+                                         attrib={'name': f"OTL_{class_.name}Type", 'abstract': "false",
+                                                 '{http://fdo.osgeo.org/schemas}geometryName': "Geometry"})
             annotation = ET.SubElement(complex_type, "{http://www.w3.org/2001/XMLSchema}annotation")
             documentation = ET.SubElement(annotation, "{http://www.w3.org/2001/XMLSchema}documentation")
             documentation.text = class_instance.__doc__
             complex_content = ET.SubElement(complex_type, "{http://www.w3.org/2001/XMLSchema}complexContent")
-            extension = ET.SubElement(complex_content, "{http://www.w3.org/2001/XMLSchema}extension", base="gml:AbstractFeatureType")
+            extension = ET.SubElement(complex_content, "{http://www.w3.org/2001/XMLSchema}extension",
+                                      attrib={'base': "{http://www.opengis.net/gml}AbstractFeatureType"})
             attribute_sequence = ET.SubElement(extension, "{http://www.w3.org/2001/XMLSchema}sequence")
 
 
@@ -76,22 +95,14 @@ class XSDCreator:
             restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:int")
 
             # TODO use _geometry_types
-            geometry = ET.SubElement(attribute_sequence, "{http://www.w3.org/2001/XMLSchema}element", name="Geometry", type="gml:AbstractGeometryType", fdo_hasMeasure="false", fdo_hasElevation="true", fdo_srsName="Default", fdo_geometricTypes="point multipoint polygon multipolygon ", fdo_geometryTypes="point multipoint polygon multipolygon ")
+            geo_types = ' '.join(class_instance._geometry_types).replace(' Z', '').lower()
+            geometry = ET.SubElement(
+                attribute_sequence, "{http://www.w3.org/2001/XMLSchema}element",
+                attrib= {'name': "Geometry", 'type': "{http://www.opengis.net/gml}AbstractGeometryType", '{http://fdo.osgeo.org/schemas}hasMeasure': "false", '{http://fdo.osgeo.org/schemas}hasElevation': "true", '{http://fdo.osgeo.org/schemas}srsName': "Default", '{http://fdo.osgeo.org/schemas}geometricTypes': geo_types, '{http://fdo.osgeo.org/schemas}geometryTypes': geo_types}
+            )
             annotation = ET.SubElement(geometry, "{http://www.w3.org/2001/XMLSchema}annotation")
             documentation = ET.SubElement(annotation, "{http://www.w3.org/2001/XMLSchema}documentation")
             documentation.text = "Default geometry property"
-
-            #           <xs:element name="afmeting_breedte" minOccurs="0" default="">
-            #             <xs:annotation>
-            #               <xs:documentation>De breedte in millimeter.</xs:documentation>
-            #             </xs:annotation>
-            #             <xs:simpleType>
-            #               <xs:restriction base="xs:decimal">
-            #                 <xs:totalDigits value="19" />
-            #                 <xs:fractionDigits value="10" />
-            #               </xs:restriction>
-            #             </xs:simpleType>
-            #           </xs:element>
 
             attribute_names_of_subset = [attribuut.name for attribuut in collector.find_attributes_by_class(class_)]
             class_instance.fill_with_dummy_data()
@@ -120,15 +131,18 @@ class XSDCreator:
                 annotation = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}annotation")
                 documentation = ET.SubElement(annotation, "{http://www.w3.org/2001/XMLSchema}documentation")
                 documentation.text = definition
-                simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
-                restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:decimal")
-                total_digits = ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}totalDigits", value="19")
-                fraction_digits = ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}fractionDigits", value="10")
 
+                simple_type = create_type_from_attribute_in_element(element, attr_instance)
 
-        tree.write(xsd_path)
+        tree.write(xsd_path, encoding='utf-8', xml_declaration=True, method='xml')
 
 def test_create_xsd_from_subset():
     kast_path = Path(__file__).parent / 'subset_wegkantkast.db'
+    created_path = Path(__file__).parent / 'created_wegkantkast.xsd'
 
-    XSDCreator.create_xsd_from_subset(kast_path, Path(__file__).parent / 'created_wegkantkast.xsd')
+    XSDCreator.create_xsd_from_subset(kast_path, created_path)
+
+    result = ET.tostring(ET.parse(created_path).getroot())
+    expected = ET.tostring(ET.parse(Path(__file__).parent / 'xsd_export_wegkantkast.xsd').getroot())
+
+    assert expected == result
