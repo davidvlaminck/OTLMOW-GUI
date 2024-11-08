e8ae13ad6719f9fe6b562bc50657f2eec189d4e5
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-11-08 14:46:42 +0100

    improvement

---
+++ Domain/XSDCreator.py
@@ -0,0 +1,163 @@
+import xml.etree.cElementTree as ET
+from pathlib import Path
+from xml.etree.ElementTree import Element
+
+from otlmow_converter.DotnotationHelper import DotnotationHelper
+from otlmow_model.OtlmowModel.BaseClasses.BooleanField import BooleanField
+from otlmow_model.OtlmowModel.BaseClasses.DateField import DateField
+from otlmow_model.OtlmowModel.BaseClasses.DateTimeField import DateTimeField
+from otlmow_model.OtlmowModel.BaseClasses.FloatOrDecimalField import FloatOrDecimalField
+from otlmow_model.OtlmowModel.BaseClasses.IntegerField import IntegerField
+from otlmow_model.OtlmowModel.BaseClasses.KeuzelijstField import KeuzelijstField
+from otlmow_model.OtlmowModel.BaseClasses.OTLObject import dynamic_create_instance_from_uri, OTLAttribuut, OTLObject
+from otlmow_model.OtlmowModel.BaseClasses.StringField import StringField
+from otlmow_model.OtlmowModel.BaseClasses.TimeField import TimeField
+from otlmow_model.OtlmowModel.Helpers.OTLObjectHelper import is_relation
+from otlmow_modelbuilder.OSLOCollector import OSLOCollector
+
+
+def create_type_from_attribute_in_element(element: Element, attr_instance: OTLAttribuut):
+    if attr_instance.field == FloatOrDecimalField:
+        simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:decimal")
+        ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}totalDigits", value="19")
+        ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}fractionDigits", value="10")
+    elif attr_instance.field == BooleanField:
+        simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:boolean")
+    elif attr_instance.field in {StringField, DateField, DateTimeField, TimeField}:
+        simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:string")
+        ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}maxLength", value="9999")
+    elif issubclass(attr_instance.field, KeuzelijstField):
+        simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:string")
+        element.attrib.update({'default': '-'})
+        max_length = 1
+        for keuze, keuzelijstwaarde in attr_instance.field.options.items():
+            if keuzelijstwaarde.status != 'ingebruik':
+                continue
+            ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}enumeration", value=keuze)
+            max_length = max(max_length, len(keuze))
+        ET.SubElement(restriction, "{http://www.w3.org/2001/XMLSchema}maxLength", value=str(max_length))
+    elif issubclass(attr_instance.field, IntegerField):
+        simple_type = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        restriction = ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:int")
+    else:
+        raise NotImplementedError(f"Field {attr_instance.field} not implemented")
+
+
+class XSDCreator:
+    @classmethod
+    def create_xsd_from_subset(cls, subset_path: Path, xsd_path: Path, model_directory: Path = None) -> None:
+        collector = OSLOCollector(subset_path)
+        collector.collect_all(include_abstract=True)
+
+        ET.register_namespace("xs", 'http://www.w3.org/2001/XMLSchema')
+        ET.register_namespace("gml", 'http://www.opengis.net/gml')
+        ET.register_namespace("Schema1", 'http://fdo.osgeo.org/schemas/feature/Schema1')
+        ET.register_namespace("fdo", 'http://fdo.osgeo.org/schemas')
+
+        schema = ET.Element("{http://www.w3.org/2001/XMLSchema}schema", targetNamespace="http://fdo.osgeo.org/schemas/feature/Schema1",  elementFormDefault="qualified", attributeFormDefault="unqualified" )
+        annotation = ET.SubElement(schema, "{http://www.w3.org/2001/XMLSchema}annotation")
+        documentation = ET.SubElement(annotation, "{http://www.w3.org/2001/XMLSchema}documentation")
+        documentation.text = "Default schema"
+        tree = ET.ElementTree(schema)
+
+        classes = collector.classes
+        for class_ in classes:
+            if class_.abstract:
+                continue
+            class_instance = dynamic_create_instance_from_uri(class_.objectUri, model_directory=model_directory)
+            if is_relation(class_instance):
+                continue
+
+            class_element = ET.SubElement(
+                schema, "{http://www.w3.org/2001/XMLSchema}element",
+                attrib={'name': f"OTL_{class_.name}",
+                        'type': f"http://fdo.osgeo.org/schemas/feature/Schema1/OTL_{class_.name}Type",
+                        'abstract': "false", 'substitutionGroup': "http://www.opengis.net/gml/_Feature"})
+            key = ET.SubElement(class_element, "{http://www.w3.org/2001/XMLSchema}key", name=f"OTL_{class_.name}Key")
+            ET.SubElement(key, "{http://www.w3.org/2001/XMLSchema}selector", xpath=f".//OTL_{class_.name}")
+            ET.SubElement(key, "{http://www.w3.org/2001/XMLSchema}field", xpath="FeatId")
+
+
+
+            complex_type = ET.SubElement(schema, "{http://www.w3.org/2001/XMLSchema}complexType",
+                                         attrib={'name': f"OTL_{class_.name}Type", 'abstract': "false",
+                                                 '{http://fdo.osgeo.org/schemas}geometryName': "Geometry"})
+            annotation = ET.SubElement(complex_type, "{http://www.w3.org/2001/XMLSchema}annotation")
+            documentation = ET.SubElement(annotation, "{http://www.w3.org/2001/XMLSchema}documentation")
+            documentation.text = class_instance.__doc__
+            complex_content = ET.SubElement(complex_type, "{http://www.w3.org/2001/XMLSchema}complexContent")
+            extension = ET.SubElement(complex_content, "{http://www.w3.org/2001/XMLSchema}extension",
+                                      attrib={'base': "http://www.opengis.net/gml/AbstractFeatureType"})
+            attribute_sequence = ET.SubElement(extension, "{http://www.w3.org/2001/XMLSchema}sequence")
+
+            cls.add_feat_id_in_attribute_sequence(attribute_sequence)
+
+            cls.add_geometry_in_attribute_sequence(attribute_sequence, class_instance)
+
+            attribute_names_in_subset = [attribuut.name for attribuut in collector.find_attributes_by_class(class_)]
+            if 'geometry' in attribute_names_in_subset:
+                attribute_names_in_subset.remove('geometry')
+            class_instance.fill_with_dummy_data()
+
+            cls.add_otl_attributes_in_attribute_sequence(attribute_names_in_subset, attribute_sequence, class_instance)
+
+        tree.write(xsd_path, encoding='utf-8', xml_declaration=True, method='xml')
+
+    @classmethod
+    def add_geometry_in_attribute_sequence(cls, attribute_sequence: Element, class_instance: OTLObject) -> None:
+        geo_types = ' '.join(class_instance._geometry_types).replace(' Z', '').lower()
+        geometry = ET.SubElement(
+            attribute_sequence, "{http://www.w3.org/2001/XMLSchema}element",
+            attrib={'name': "Geometry", 'type': "http://www.opengis.net/gml/AbstractGeometryType",
+                    '{http://fdo.osgeo.org/schemas}hasMeasure': "false",
+                    '{http://fdo.osgeo.org/schemas}hasElevation': "true",
+                    '{http://fdo.osgeo.org/schemas}srsName': "Default",
+                    '{http://fdo.osgeo.org/schemas}geometricTypes': geo_types,
+                    '{http://fdo.osgeo.org/schemas}geometryTypes': geo_types}
+        )
+        annotation = ET.SubElement(geometry, "{http://www.w3.org/2001/XMLSchema}annotation")
+        documentation = ET.SubElement(annotation, "{http://www.w3.org/2001/XMLSchema}documentation")
+        documentation.text = "Default geometry property"
+
+    @classmethod
+    def add_feat_id_in_attribute_sequence(cls, attribute_sequence: Element):
+        feat_id = ET.SubElement(attribute_sequence, "{http://www.w3.org/2001/XMLSchema}element", name="FeatId",
+                                readOnly="true", autogenerated="true")
+        annotation = ET.SubElement(feat_id, "{http://www.w3.org/2001/XMLSchema}annotation")
+        documentation = ET.SubElement(annotation, "{http://www.w3.org/2001/XMLSchema}documentation")
+        documentation.text = "Default identity property"
+        simple_type = ET.SubElement(feat_id, "{http://www.w3.org/2001/XMLSchema}simpleType")
+        ET.SubElement(simple_type, "{http://www.w3.org/2001/XMLSchema}restriction", base="xs:int")
+
+    @classmethod
+    def add_otl_attributes_in_attribute_sequence(cls, attribute_names_in_subset: [str], attribute_sequence: Element,
+                                                 class_instance: OTLObject) -> None:
+        for dotnotation, _ in DotnotationHelper.list_attributes_and_values_by_dotnotation(
+                class_instance, waarde_shortcut=True):
+            first_in_dotnotation = dotnotation
+            if '.' in first_in_dotnotation:
+                first_in_dotnotation = first_in_dotnotation.split('.')[0]
+            if '[]' in first_in_dotnotation:
+                first_in_dotnotation = first_in_dotnotation[:-2]
+            if first_in_dotnotation not in attribute_names_in_subset:
+                continue
+
+            attr_instance = DotnotationHelper.get_attribute_by_dotnotation(class_instance, dotnotation)
+            definition = attr_instance.definition
+            if hasattr(attr_instance.owner, '_parent'):
+                parent_attribute_for_waarde_shortcut = attr_instance.owner._parent
+                if parent_attribute_for_waarde_shortcut.field.waarde_shortcut_applicable:
+                    dotnotation = DotnotationHelper.get_dotnotation(parent_attribute_for_waarde_shortcut)
+                    definition = parent_attribute_for_waarde_shortcut.definition
+
+            element = ET.SubElement(attribute_sequence, "{http://www.w3.org/2001/XMLSchema}element",
+                                    name=dotnotation.replace('.', '_'), minOccurs="0", default="")
+            annotation = ET.SubElement(element, "{http://www.w3.org/2001/XMLSchema}annotation")
+            documentation = ET.SubElement(annotation, "{http://www.w3.org/2001/XMLSchema}documentation")
+            documentation.text = definition
+
+            create_type_from_attribute_in_element(element, attr_instance)
---
+++ UnitTests/XSD-Subsets/XSDCreator_test.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import xml.etree.cElementTree as ET
+
+from Domain.XSDCreator import XSDCreator
+
+
+def test_create_xsd_from_subset_wegkantkast():
+    kast_path = Path(__file__).parent / 'subset_wegkantkast.db'
+    created_path = Path(__file__).parent / 'created_wegkantkast.xsd'
+
+    XSDCreator.create_xsd_from_subset(kast_path, created_path)
+
+    result = ET.tostring(ET.parse(created_path).getroot())
+    expected = ET.tostring(ET.parse(Path(__file__).parent / 'xsd_export_wegkantkast.xsd').getroot())
+
+    assert expected == result
+
+
+def test_create_xsd_from_subset_testclass():
+    kast_path = Path(__file__).parent / 'subset_testclass.db'
+    created_path = Path(__file__).parent / 'created_testclass.xsd'
+
+    model_directory = Path(__file__).parent.parent / 'TestModel'
+
+    XSDCreator.create_xsd_from_subset(kast_path, created_path, model_directory)
+
+    result = ET.tostring(ET.parse(created_path).getroot())
+    expected = ET.tostring(ET.parse(Path(__file__).parent / 'xsd_export_testclass.xsd').getroot())
+
+    assert expected == result
