93f361e4ac3eab2bdfc33c086327119d19aac124
christiaan <christiaan.vanbergen.btf@gmail.com>
2024-12-17 11:51:52 +0100

    Fixed OTLLogger.py formating of debug messages

--- Domain/logger/OTLLogger.py
+++ Domain/logger/OTLLogger.py
@@ -6,13 +6,10 @@ class OTLLogger:
 
     @classmethod
     def init(cls,logging_file: Path):
-        logging.basicConfig(
-            format='%(asctime)s %(levelname)-8s %(message)s',
-            level=logging.DEBUG,
-            datefmt='%Y-%m-%d %H:%M:%S')
-
         file_handler = logging.FileHandler(logging_file)
         file_handler.setLevel(logging.DEBUG)
         logger = logging.getLogger()
         logger.addHandler(file_handler)
         logger.setLevel(logging.DEBUG)
+        logger.handlers[0].setFormatter(logging.Formatter(fmt='%(asctime)s ln %(lineno)-4d:%(filename)-25s %(levelname)-8s %(message)s',
+             datefmt='%Y-%m-%d %H:%M:%S'))
