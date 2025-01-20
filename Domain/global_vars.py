e2ef7b2789447769575a84acc474183f1cfd6130
christiaan <christiaan.vanbergen.btf@gmail.com>
2025-01-20 15:54:17 +0100

    Changed supported_file_formats to only include SDF on Windows systems

--- Domain/global_vars.py
+++ Domain/global_vars.py
@@ -1,11 +1,17 @@
 import os
+import sys
 from pathlib import Path
 from typing import Optional
 
 projects = []
 current_project = None
 otl_wizard = None
-supported_file_formats = {'Excel':"xlsx", 'CSV':"csv", 'JSON':'json','GeoJSON': 'geojson','SDF':'sdf'}
+# SDF files are only supported in Windows systems
+if "win" in sys.platform:
+    supported_file_formats = {'Excel':"xlsx", 'CSV':"csv", 'JSON':'json','GeoJSON': 'geojson','SDF':'sdf'}
+else:
+    supported_file_formats = {'Excel': "xlsx", 'CSV': "csv", 'JSON': 'json', 'GeoJSON': 'geojson'}
+
 external_toegekendDoor_label = "OTL_wizard_2"
 last_subset_selected_dir: Optional[Path] = None
 FDO_toolbox_path_str =  'C:\\Program Files\\FDO Toolbox\\FdoCmd.exe'
