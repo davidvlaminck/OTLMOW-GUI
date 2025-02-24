060707400d330a1d62ce2304fa5bd857dd94e16b
christiaan <christiaan.vanbergen.btf@gmail.com>
2025-02-24 14:39:40 +0100

    Added turning off physics after mouse hover over webview

--- GUI/screens/DataVisualisationScreen.py
+++ GUI/screens/DataVisualisationScreen.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import replace
 from typing import List
 
 import qtawesome as qta
@@ -136,6 +137,8 @@ class DataVisualisationScreen(Screen):
         OTLLogger.logger.debug(
             f"Executing DataVisualisationScreen.reload_html() for project {global_vars.current_project.eigen_referentie} ({object_count} objects)",
             extra={"timing_ref": f"reload_html_{global_vars.current_project.eigen_referentie}"})
+        js_code = 'newOptions = {"physics":{"enabled":false}};\nnetwork.setOptions(newOptions);\n'
+        self.view.page().runJavaScript(js_code)
 
     def load_assets(self) -> List[OTLObject]:
         return RelationChangeDomain.get_quicksave_instances()
@@ -162,4 +165,32 @@ class DataVisualisationScreen(Screen):
             PyVisWrapper().show(list_of_objects=objects_in_memory,
                                 html_path=Path(html_loc), launch_html=False)
             os.chdir(previous_cwd)
+
+            self.modify_html(html_loc)
+
             self.view.setHtml(open(html_loc).read())
+
+    def modify_html(cls, file_path: Path) -> None:
+        with open(file_path) as file:
+            file_data = file.readlines()
+
+        replace_index = -1
+        for index, line in enumerate(file_data):
+            if "drawGraph();" in line:
+                replace_index = index
+
+        if replace_index > 0:
+            file_data[replace_index] = file_data[replace_index].replace("drawGraph();","var network = drawGraph();")
+            file_data.insert(replace_index,"var container = document.getElementById('mynetwork');\n")
+            file_data.insert(replace_index+1,
+                             "var isPhysicsOn = true;\n")
+            file_data.insert(replace_index+2,
+                             "function disablePhysics(){\n")
+            file_data.insert(replace_index+3,"if(isPhysicsOn){")
+            file_data.insert(replace_index+4,'newOptions={"physics":{"enabled":false}};\n')
+            file_data.insert(replace_index+5,"network.setOptions(newOptions)};\n;")
+            file_data.insert(replace_index + 6, "isPhysicsOn = false;\n};\n")
+            file_data.insert(replace_index+7,"container.addEventListener('mouseover', disablePhysics);\n")
+        with open(file_path, 'w') as file:
+            for line in file_data:
+                file.write(line)
