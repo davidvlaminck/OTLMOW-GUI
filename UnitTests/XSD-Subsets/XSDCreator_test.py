2c3175fa14cdb6857a4d71484cfc56d69973031f
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2025-07-31 11:26:45 +0200

    commented to remove an unnecessary warning

--- UnitTests/XSD-Subsets/XSDCreator_test.py
+++ UnitTests/XSD-Subsets/XSDCreator_test.py
@@ -13,7 +13,7 @@ def root_directory() -> Path:
 
 #TODO fix test after discussion with David
 @unittest.skip('Difference between old OTL-wizard generated XSD and new python generate XSD')
-@pytest.mark.asyncio
+# @pytest.mark.asyncio
 async def test_create_xsd_from_filtered_subset_slagbomen(root_directory,cleanup_after_creating_a_file_to_delete):
     # SETUP
     kast_path = root_directory / 'UnitTests' /'test_files' / 'input' / 'voorbeeld-slagboom.db'
